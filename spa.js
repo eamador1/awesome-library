4d8fcbbd8afef4bd8f3627421c709f8e7b5068b4
erwin amador <eamador0305@gmail.com>
2023-08-10 18:35:10 -0600

    make spa.js work

--- spa.js
+++ spa.js
@@ -4,17 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const $awesome = document.querySelector('.awesome');
     const $new = document.querySelector('.new');
     const $add = document.querySelector('.add');
+    const $contact = document.querySelector('.contact');
+    const $contactInfo = document.querySelector('.contactInform');
+    const $footer = document.querySelector('#footer');
     
   
     $list.addEventListener('click', () => {
     $library.classList.add('active');
-    $add.classList.remove('active');
     $awesome.classList.add('active');
+    $footer.classList.add('active');
+    $add.classList.remove('active');
+    $contactInfo.classList.remove('active');   
     });
 
     $new.addEventListener('click', () => {
     $add.classList.add('active');
+    $footer.classList.add('active');
+    $library.classList.remove('active');
+    $awesome.classList.remove('active');
+    $contactInfo.classList.remove('active');
+    });
+
+    $contact.addEventListener('click', () => {
+    $contactInfo.classList.add('active');
+    $footer.classList.add('active');
+    $add.classList.remove('active');
     $library.classList.remove('active');
     $awesome.classList.remove('active');
     });
-  });
+  }); 
