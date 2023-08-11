387b627317bd04e133234355231be657ee159889
DamianDib9 <121461459+DamianDib9@users.noreply.github.com>
2023-08-10 22:52:33 -0300

    Update spa.js
    fixed classes for proper container displaying

--- spa.js
+++ spa.js
@@ -1,35 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const $list = document.querySelector('.list');
-    const $library = document.querySelector('.displayLibrary');
-    const $awesome = document.querySelector('.awesome');
-    const $new = document.querySelector('.new');
-    const $add = document.querySelector('.add');
-    const $contact = document.querySelector('.contact');
-    const $contactInfo = document.querySelector('.contactInform');
-    const $footer = document.querySelector('#footer');
-    
-  
-    $list.addEventListener('click', () => {
+  const $list = document.querySelector('.list');
+  const $library = document.querySelector('.displayLibrary');
+  const $awesome = document.querySelector('.awesome');
+  const $new = document.querySelector('.new');
+  const $add = document.querySelector('.add');
+  const $contact = document.getElementById('contactButton');
+  const $contactInfo = document.querySelector('.contactInform');
+  const $footer = document.querySelector('.footer');
+
+  $list.addEventListener('click', () => {
     $library.classList.add('active');
     $awesome.classList.add('active');
     $footer.classList.add('active');
     $add.classList.remove('active');
-    $contactInfo.classList.remove('active');   
-    });
+    $contactInfo.classList.remove('active');
+  });
 
-    $new.addEventListener('click', () => {
+  $new.addEventListener('click', () => {
     $add.classList.add('active');
     $footer.classList.add('active');
     $library.classList.remove('active');
     $awesome.classList.remove('active');
     $contactInfo.classList.remove('active');
-    });
+  });
 
-    $contact.addEventListener('click', () => {
+  $contact.addEventListener('click', () => {
     $contactInfo.classList.add('active');
     $footer.classList.add('active');
     $add.classList.remove('active');
     $library.classList.remove('active');
     $awesome.classList.remove('active');
-    });
-  }); 
+  });
+});
